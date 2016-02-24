2be335defa91426d828d33bce1feae8c20e55925
Johan Bergström <johan_the_man89@hotmail.com>
2016-02-24 12:58:03 +0100

    Added Website Masterpage and started class diagram

---
+++ E-Handelssite/Användare.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace E_Handelssite
+{
+    public abstract class Användare
+    {
+        public string UserName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public bool IsAdmin
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
