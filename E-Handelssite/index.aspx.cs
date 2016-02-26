74f75d1ba8dc11559d36f3938c2bc3e51f240eee
Johan Bergström <johan_the_man89@hotmail.com>
2016-02-26 12:33:33 +0100

    Added methods and bootstrap apparently

--- E-Handelssite/index.aspx.cs
+++ E-Handelssite/index.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Kundbibliotek;
 
 namespace E_Handelssite
 {
@@ -11,7 +12,40 @@ namespace E_Handelssite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            LoadCarousel();
+            LoadMostPurchased();
 
         }
+
+        private void LoadMostPurchased()
+        {
+        }
+
+        private void LoadCarousel()
+        {
+        }
+
+        private void RemovePictureCarousel(Användare user)
+        {
+            if (user.IsAdmin)
+            {
+                //Kod för att tabort bild i karusellen
+            }
+        }
+        private void AddPictureCarousel(Användare user)
+        {
+            if (user.IsAdmin)
+            {
+                //Kod för att lägga till bild i karusellen.
+            }
+        }
+        private void ChangeMostPurchased(Användare user, int cellID)
+        {
+            if (user.IsAdmin)
+            {
+                //Kod för att ändra bild i mest köpta.
+            }
+        }
+        
     }
 }
